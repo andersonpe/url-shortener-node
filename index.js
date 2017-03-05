8f3fb9398293931bde014f986c609f8c62236eff
Bradley Price <bradleyprice@gmail.com>
2017-03-05 13:19:25 -0600

    Add code and setup server for shortening service

---
+++ index.js
@@ -0,0 +1,43 @@
+const Hapi = require('hapi')
+const server = new Hapi.Server()
+const routes = require('./routes')
+const mongoose = require('mongoose')
+const mongoUri = process.env.MONGO_URI || 'mongodb://localhost/shortio'
+const inert = require('inert')
+
+const options = {
+  server: {
+    socketOptions: {
+      keepAlive: 300000,
+      connectTimeoutMS: 30000
+    }
+  },
+  replset: {
+    socketOptions: {
+      keepAlive: 300000,
+      connectTimeoutMS: 3000
+    }
+  }
+}
+
+mongoose.connect(mongoUri, options)
+
+const db = mongoose.connection
+
+server.connection({
+  port: process.env.PORT || 3000,
+  routes: { cors: true }
+})
+
+server.register(inert, (err) => {
+  db.on('error', console.error.bind(console, 'connection error: '))
+    .once('open', () => {
+      server.route(routes)
+
+      server.start((err) => {
+        if (err) throw err
+
+        console.log(`Server running on port ${server.info.port}`)
+      })
+    })
+})
